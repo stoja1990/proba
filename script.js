96c0fdc8f151274491c6c4d60c03bf9bc16bbaa3
stoja1990 <cokula1990@gmail.com>
2021-03-22 10:12:50 +0100

    Dodat EventListener

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('memla')
+let natpis = document.querySelector('h1')
+let dugme = document.querySelector('button')
+
+dugme.addEventListener('click',function () {
+	natpis.style.color = 'blue';
+	dugme.style.color = 'red'
+})
