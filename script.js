c1cb577ccd0f4eb297bacb30e6b7dff655412890
stoja1990 <cokula1990@gmail.com>
2021-03-22 10:53:16 +0100

    Dodat toggle classList

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let natpis = document.querySelector('h1')
+let natpis = document.querySelector('.prva')
 let dugme = document.querySelector('button')
 
+
+
 dugme.addEventListener('click',function () {
-	natpis.style.color = 'blue';
-	dugme.style.color = 'red'
+	natpis.classList.toggle("boja")
 })
